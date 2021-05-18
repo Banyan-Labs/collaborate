ed6632537078941d2eb637a0ef6d9879b64ff566
joseph-caldwell <67066124+joseph-caldwell@users.noreply.github.com>
2021-05-18 10:53:21 -0500

    just getting started really

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
 import { Button } from '@adobe/react-spectrum'
-
+import Image from 'react-bootstrap/Image'
 
 
 export default class Hero extends Component {
@@ -8,7 +9,7 @@ export default class Hero extends Component {
     render() {
         return (
             <div>
-
+                <Image src="./images/hero1.jpg/100px250" fluid />
             </div>
         )
     }
