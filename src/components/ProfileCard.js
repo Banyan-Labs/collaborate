6271492688c1b4050d75a6dfd95af93c0984a109
joseph-caldwell <67066124+joseph-caldwell@users.noreply.github.com>
2021-05-25 09:37:09 -0500

    default cards

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, CardGroup, Image } from 'react-bootstrap';
-
+import ProfilePicture from "../images/userProfile.png";
 
 class ProfileCard extends React.Component {
     state = {
@@ -13,90 +13,49 @@ class ProfileCard extends React.Component {
     render() {
         return (
             <div>
-                <div>
+                <div className="mb-5">
                     <h1 style={{ textAlign: 'center' }}>Profile Page</h1>
                 </div>
                 {/* // <Button type='submit' onClick={() => this.AddCard()}>Add Profile</Button> */}
                 <CardGroup>
-                    <Card>
-                        <Image src="https://static.pexels.com/photos/296886/pexels-photo-296886.jpeg" responsive />
+                    <Card className="mr-3 mb-5">
+                        <Card.Img variant="top" src={ProfilePicture} responsive />
                         <Card.Body>
                             <Card.Title style={{ textAlign: 'center' }}>{this.state.userName}</Card.Title>
                             <Card.Text>
                                 {this.state.aboutUser}
                             </Card.Text>
                         </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Edit Profile</small>
+                        <Card.Footer style={{ textAlign: 'center' }}>
+                            <small className="text-muted" >Edit Profile</small>
                         </Card.Footer>
                     </Card>
-                    <Card>
-                        <Image src="https://static.pexels.com/photos/296886/pexels-photo-296886.jpeg" responsive />
+                    <Card className="mr-3 mb-5">
+                        <Card.Img variant="top" src={ProfilePicture} responsive />
                         <Card.Body>
                             <Card.Title style={{ textAlign: 'center' }}>{this.state.userName}</Card.Title>
                             <Card.Text>
                                 {this.state.aboutUser}
                             </Card.Text>
                         </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Edit Profile</small>
+                        <Card.Footer style={{ textAlign: 'center' }}>
+                            <small className="text-muted" >Edit Profile</small>
                         </Card.Footer>
                     </Card>
-                    <Card>
-                        <Image src="https://static.pexels.com/photos/296886/pexels-photo-296886.jpeg" responsive />
+                    <Card className="mr-3 mb-5">
+                        <Card.Img variant="top" src={ProfilePicture} responsive />
                         <Card.Body>
                             <Card.Title style={{ textAlign: 'center' }}>{this.state.userName}</Card.Title>
                             <Card.Text>
                                 {this.state.aboutUser}
                             </Card.Text>
                         </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Edit Profile</small>
+                        <Card.Footer style={{ textAlign: 'center' }}>
+                            <small className="text-muted" >Edit Profile</small>
                         </Card.Footer>
                     </Card>
                 </CardGroup>
-                {/* <CardGroup>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-      </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This card has even longer content than the first to
-                                show that equal height action.
-      </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                </CardGroup> */}
+
             </div>
 
         )
