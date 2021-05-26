e371eb06aa53b6a68de794c109dec5d0bdf4b328
Austin Hill <Austinkinghill@gmail.com>
2021-05-26 09:16:28 -0700

    create site_data file with example in navbar

---
+++ src/site-data.js
@@ -0,0 +1,32 @@
+export const SITE_DATA = {
+    developerName:"TEST DEV",
+    logo: "https://banyanlabs.io/static/Logo-ba022e637f1be3fe28d39b0560ae901b.png",
+    contactLinks: {
+        github:"",
+        twitter:"",
+        linkedIn:"",
+    },
+    projects: [
+        {   
+            projectName: "Project 1",
+            projectDescription: "One line project description",
+            thumbnailImage: "",
+            githubUrl: "",
+            liveSiteUrl: "",
+        },
+        {   
+            projectName: "Project 2",
+            projectDescription: "One line project description",
+            thumbnailImage: "",
+            githubUrl: "",
+            liveSiteUrl: "",
+        },
+        {   
+            projectName: "Project 3",
+            projectDescription: "One line project description",
+            thumbnailImage: "",
+            githubUrl: "",
+            liveSiteUrl: "",
+        },
+    ]
+}
