f352193f751c2e33214a701d7c3187f8fc472311
tamaraeaton <80291920+tamaraeaton@users.noreply.github.com>
2021-05-27 12:29:55 -0700

    COL-36-TH

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,32 +1,32 @@
 
 import React from 'react';
-import {SITE_DATA} from '../../site-data'
+import { SITE_DATA } from '../../site-data'
 import * as ReactBootStrap from 'react-bootstrap';
 
 class Navbar extends React.Component {
   render() {
-    return(
+    return (
       <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      {
-        SITE_DATA.logo === "" ?
-        null:
-        <ReactBootStrap.Navbar.Brand href="/">
-        <img src={SITE_DATA.logo} alt="my-logo" style={{width:100}}/>
-        </ReactBootStrap.Navbar.Brand>
-      }
+        {
+          SITE_DATA.logo === "" ?
+            null :
+            <ReactBootStrap.Navbar.Brand href="/">
+              <img src={SITE_DATA.logo} alt="my-logo" style={{ width: 100 }} />
+            </ReactBootStrap.Navbar.Brand>
+        }
 
         <ReactBootStrap.Navbar.Brand href="/">{SITE_DATA.developerName}</ReactBootStrap.Navbar.Brand>
         <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-        <ReactBootStrap.Nav className="mr-auto">
-          <ReactBootStrap.Nav.Link href="/Home">Home</ReactBootStrap.Nav.Link>
-          <ReactBootStrap.Nav.Link href="/Contacts">Contacts</ReactBootStrap.Nav.Link>
-          <ReactBootStrap.Nav.Link href="/Projects">Projects</ReactBootStrap.Nav.Link>          
-        </ReactBootStrap.Nav>
-        <ReactBootStrap.Nav>
-        </ReactBootStrap.Nav>
+          <ReactBootStrap.Nav className="mr-auto">
+            <ReactBootStrap.Nav.Link href="/Home">Home</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="/Contacts">Contacts</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="/Projects">Projects</ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+          <ReactBootStrap.Nav>
+          </ReactBootStrap.Nav>
         </ReactBootStrap.Navbar.Collapse>
-      </ReactBootStrap.Navbar>      
+      </ReactBootStrap.Navbar>
     )
   }
 }
