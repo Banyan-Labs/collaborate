1a0a051ca982d78740a17a899ca93791cc189606
joseph-caldwell <67066124+joseph-caldwell@users.noreply.github.com>
2021-06-02 18:27:15 -0500

    project card complete

---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class ProjectsSection extends Component {
+    render() {
+        return (
+            <div className="mt-3">
+                <div className="mb-5 border border-bottom-3 border-warning">
+                    <h1 style={{ textAlign: 'center', color: 'indigo', fontFamily: 'Algerian' }}>Project Page</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -27,36 +27,7 @@ function ProjectCard(props) {
                         <a className="btn btn-primary mr-5" href={SITE_DATA.projects[2].liveSiteUrl}  >Banyan Labs</a>
                     </Card.Footer>
                 </Card>
-                <Card className="mr-3 mb-5">
-                    <Card.Img className=" border border-info border-4" variant="top" src={SITE_DATA.projects[1].thumbnailImage} responsive />
-                    <Card.Body className=" border border-info border-4">
-                        <Card.Title className=" border border-primary border-bottom-4" style={{ textAlign: 'center', color: 'navy' }}><h2>{SITE_DATA.projects[1].projectName}</h2></Card.Title>
-                        <Card.Text className='color-info'>
-                            <h5 style={{ color: 'navy' }}>About project:</h5>
-                            <p className='color-info'>{SITE_DATA.projects[1].projectDescription}</p>
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer className="border border-info border-4" style={{ textAlign: 'center', fontFamily: 'Algerian' }}>
-                        <a className="btn btn-primary mr-5" href={SITE_DATA.projects[2].githubUrl} >Banyan Labs Github</a>
-                        <hr />
-                        <a className="btn btn-primary mr-5" href={SITE_DATA.projects[2].liveSiteUrl}  >Banyan Labs</a>
-                    </Card.Footer>
-                </Card>
-                <Card className="mr-3 mb-5">
-                    <Card.Img className=" border border-info border-4" variant="top" src={SITE_DATA.projects[2].thumbnailImage} responsive />
-                    <Card.Body className=" border border-info border-4">
-                        <Card.Title className=" border border-primary border-bottom-4" style={{ textAlign: 'center', color: 'navy' }}><h2>{SITE_DATA.projects[2].projectName}</h2></Card.Title>
-                        <Card.Text className='color-info'>
-                            <h5 style={{ color: 'navy' }}>About project:</h5>
-                            <p className='color-info' >{SITE_DATA.projects[2].projectDescription}</p>
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer className="border border-info border-4" style={{ textAlign: 'center', fontFamily: 'Algerian' }}>
-                        <a className="btn btn-primary mr-5" href={SITE_DATA.projects[2].githubUrl} >Banyan Labs Github</a>
-                        <hr />
-                        <a className="btn btn-primary mr-5" href={SITE_DATA.projects[2].liveSiteUrl}  >Banyan Labs</a>
-                    </Card.Footer>
-                </Card>
+
             </CardGroup>
 
         </div>
