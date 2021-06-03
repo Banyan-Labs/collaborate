3af7cb80fef09244dc9e06ba40d2e83da7017528
tamaraeaton <80291920+tamaraeaton@users.noreply.github.com>
2021-06-03 15:08:32 -0700

    Updated last error

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Form, Button, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './contactForm.css'
+import './ContactForm.css'
 
 function ContactForm() {
     return (
