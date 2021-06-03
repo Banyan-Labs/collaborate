74257ddd95889cc2bbc86e8c1c3da89fd20f9e41
michaelcozart <78495354+michaelcozart@users.noreply.github.com>
2021-06-03 13:30:10 -0700

    add css to jumbotron

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {Jumbotron, Button} from 'react-bootstrap';
+import {Jumbotron} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {SITE_DATA} from '../../site-data'
+import './Jumbotron.css'
 
 function Jumbo() {
     return (
-        <div>
-            <Jumbotron>
+        <Jumbotron id="layout">
+            <div>
                 <h1>{SITE_DATA.jumbotronTxet.heading}</h1>
-                    <p>{SITE_DATA.jumbotronTxet.subheading}</p>
-                    <Button variant="primary" href="Profile Page">{SITE_DATA.jumbotronTxet.buttonText}</Button>                    
-            </Jumbotron>
-        </div>
-    )
+                <p>{SITE_DATA.jumbotronTxet.subheading}</p>
+                <button id='button' href="Profile Page">{SITE_DATA.jumbotronTxet.buttonText}</button>
+            </div>                    
+        </Jumbotron>
+        
+        )
 }
 
 export default Jumbo
