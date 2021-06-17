00c2dd4bf573140979c5f2d89010f842d735d367
akinghill <austinkinghill@gmail.com>
2021-06-17 09:06:47 -0700

    add prettier, config, format Contact Form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,28 +1,40 @@
-import React from 'react'
+import React from 'react';
 import { Form, Button, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './ContactForm.css'
+import './ContactForm.css';
 
 function ContactForm() {
-    return (
-        <div>
-            <Form className="contact-form">
-                <Col>
-                    <h4>Contact Me</h4>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Control className="form-input" type="text" placeholder="First Name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Control className="form-input" type="text" placeholder="Last Name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Control className="form-input" type="email" placeholder="Email" />
-                    </Form.Group>
-                    <Button id="button">Submit</Button>
-                </Col>
-            </Form>
-        </div>
-    )
+  return (
+    <div>
+      <Form className='contact-form'>
+        <Col>
+          <h4>Contact Me</h4>
+          <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Control
+              className='form-input'
+              type='text'
+              placeholder='First Name'
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Control
+              className='form-input'
+              type='text'
+              placeholder='Last Name'
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Control
+              className='form-input'
+              type='email'
+              placeholder='Email'
+            />
+          </Form.Group>
+          <Button id='button'>Submit</Button>
+        </Col>
+      </Form>
+    </div>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
